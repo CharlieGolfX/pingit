91b5c7c6dd40ef81c08057db6ea7f5f21138a563
charliegolfx <dev@cgx.no>
2025-05-29 15:57:06 +0200

    updated formatting

--- pingit.sh
+++ pingit.sh
@@ -4,6 +4,28 @@ LOGFILE="pingit.log"
 
 while true; do
     echo "----- $(date +"%H:%M:%S %d.%m.%Y") -----" >> "$LOGFILE"
-    ping -c 1 1.1.1.1 >> "$LOGFILE" 2>&1
+    PING_OUTPUT=$(ping -c 1 1.1.1.1 2>&1)
+    if echo "$PING_OUTPUT" | grep -q "1 received"; then
+        echo "$PING_OUTPUT" | awk '
+            /PING/ { 
+                print "--- 1.1.1.1 ping statistics ---"
+                print "bytes sent: 56"
+            }
+            /bytes from/ {
+                match($0, /icmp_seq=([0-9]+)/, icmp);
+                match($0, /ttl=([0-9]+)/, ttl);
+                match($0, /time=([0-9.]+)/, t);
+                print "bytes received: 64"
+                print "icmp: " icmp[1]
+                print "ttl: " ttl[1]
+                print "time: " t[1] " ms"
+            }
+        ' >> "$LOGFILE"
+    else
+        echo "--- 1.1.1.1 ping statistics ---" >> "$LOGFILE"
+        echo "Ping failed or no response received." >> "$LOGFILE"
+        echo "$PING_OUTPUT" | grep -E "unknown host|100% packet loss|unreachable|Name or service not known" >> "$LOGFILE"
+    fi
+    echo "" >> "$LOGFILE"
     sleep 5
 done
